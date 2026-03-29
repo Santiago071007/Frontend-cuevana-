93dffbe93d8885b7f39de1db493895b1fc36ea91
Santiago Torres <santiagocantor23@gmail.com>
2026-03-29 13:01:13 -0500

    Fix puerto para Render

--- app.js
+++ app.js
@@ -407,6 +407,8 @@ app.delete("/media/:id", (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
